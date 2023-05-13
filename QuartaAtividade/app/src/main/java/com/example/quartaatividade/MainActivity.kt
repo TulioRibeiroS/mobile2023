36cc838dabc39d8f3a370b47661525bfa684988f
TulioRibeiroS <44611234+TulioRibeiroS@users.noreply.github.com>
2023-05-13 14:34:09 -0300

    QuartaAtividade

---
+++ QuartaAtividade/app/src/main/java/com/example/quartaatividade/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.quartaatividade
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var IvRoller: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val BtRoller: Button = findViewById(R.id.BtRoller)
+        BtRoller.text = getString(R.string.roll)
+        BtRoller.setOnClickListener {
+            playDice()
+        }
+
+        IvRoller = findViewById(R.id.IvRoller)
+    }
+
+    private fun playDice() {
+        val randomInt = Random.nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        IvRoller.setImageResource(drawableResource)
+    }
+
+}
